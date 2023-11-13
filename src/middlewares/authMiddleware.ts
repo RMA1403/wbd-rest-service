f7c49c0542e487f4df3fcb7fe3bba191b00324aa
Rava Maulana <ravamaulana14@gmail.com>
2023-11-13 13:06:55 +0700

    feat: xml parse

--- src/middlewares/authMiddleware.ts
+++ src/middlewares/authMiddleware.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import verifyToken from "../lib/verifyToken";
-
+import axios from "axios";
+import xml2js from "xml2js";
 export default class AuthMiddleware {
   verify() {
     return async function authMiddleware(
@@ -11,11 +12,44 @@ export default class AuthMiddleware {
       try {
         const bearerToken = req.headers.authorization?.split(" ")[1] || "";
 
+        // Validate jwt token
         if (bearerToken === "") {
           return res.status(401).json({ message: "invalid token" });
         }
 
         const token = await verifyToken(bearerToken);
+
+        const soapRes = await axios.post(
+          `${process.env.SOAP_URL}/subscription`,
+          `
+            <soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/" xmlns:tns="http://services.soapserver/">
+              <soap:Header>
+                <tns:test>ularmelingkardiataspagar</tns:test>
+              </soap:Header>
+              <soap:Body>
+                <tns:checkSubscription>
+                  <idUser>1</idUser>
+                </tns:checkSubscription>
+              </soap:Body>
+            </soap:Envelope>
+        `,
+          {
+            headers: {
+              "Content-Type": "text/xml",
+            },
+          }
+        );
+
+        const xmlRes = await xml2js.parseStringPromise(soapRes.data);
+        const subscribeRes =
+          xmlRes["S:Envelope"]["S:Body"][0]["ns2:checkSubscriptionResponse"][0][
+            "return"
+          ][0];
+
+        // Check user subscription
+        if (subscribeRes !== "subscribed") {
+          return res.status(403).json({ message: "user not subscribed" });
+        }
 
         req.body.idUser = (token as any).data;
         next();
