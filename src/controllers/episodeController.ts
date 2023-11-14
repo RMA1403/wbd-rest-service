f8784d18f69b5bb9f05a0e3792d919728fc31685
Rava Maulana <ravamaulana14@gmail.com>
2023-11-14 14:27:57 +0700

    feat: initialized multer

--- src/controllers/episodeController.ts
+++ src/controllers/episodeController.ts
@@ -1,33 +1,14 @@
 import { Request, Response } from "express";
-import fs from "fs";
 
 export class EpisodeController {
-  playEpisode() {
+  createEpisode() {
     return async (req: Request, res: Response) => {
-      const { range } = req.headers;
-      if (!range) {
-        res.status(400).send("Requires range header");
-        return;
+      try {
+        return res.status(200).json({ body: req.body, files: req.files });
+      } catch (err) {
+        console.log(err);
+        return res.status(500).json({ message: "internal server error" });
       }
-
-      const audioPath = "src/storage/audiotester.mp3";
-      const audioSize = fs.statSync(audioPath).size;
-
-      const chunkSize = 512 * 10 ** 3;
-      const start = Number(range.replace(/\D/g, ""));
-      const end = Math.min(start + chunkSize, audioSize - 1);
-      const contentLength = end - start + 1;
-
-      const headers = {
-        "Content-Range": `bytes ${start}-${end}/${audioSize}`,
-        "Accept-Ranges": "bytes",
-        "Content-Length": contentLength,
-        "Content-Type": "audio/mp3",
-      };
-      res.writeHead(206, headers);
-
-      const audioStream = fs.createReadStream(audioPath, { start, end });
-      audioStream.pipe(res);
     };
   }
 }
--- src/routers/episodeRouter.ts
+++ src/routers/episodeRouter.ts
@@ -1,14 +1,30 @@
 import { Router } from "express";
 import { EpisodeController } from "../controllers/episodeController";
+import AuthMiddleware from "../middlewares/authMiddleware";
+import UploadMiddleware from "../middlewares/uploadMiddleware";
 
 export class EpisodeRouter {
+  uploadMiddleware: UploadMiddleware;
+  authMiddleware: AuthMiddleware;
+
   episodeController: EpisodeController;
 
   constructor() {
+    this.uploadMiddleware = new UploadMiddleware();
+    this.authMiddleware = new AuthMiddleware();
+
     this.episodeController = new EpisodeController();
   }
 
   getRoute() {
-    return Router().get("/audio", this.episodeController.playEpisode());
+    return (
+      Router()
+        // .use("/episode", new AuthMiddleware().verify())
+        .post(
+          "/episode",
+          this.uploadMiddleware.handleEpisodeUpload(),
+          this.episodeController.createEpisode()
+        )
+    );
   }
 }
